c8b8a6720ea718b8d655a3ace13df19f6aa5aefd
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-10-13 20:05:07 +0300

    HotFix

--- src/main/java/ua/kpi/iasa/ServletWebMarket/config/HibernateInit.java
+++ src/main/java/ua/kpi/iasa/ServletWebMarket/config/HibernateInit.java
@@ -4,16 +4,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateInit {
-    private static final SessionFactory sessionFactory;
+    private static final SessionFactory SESSION_FACTORY;
 
     private HibernateInit() {
     }
 
     static {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
+        SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+        return SESSION_FACTORY;
     }
 }
